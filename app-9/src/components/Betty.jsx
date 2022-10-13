d9c37e99e655745ad0139f6f3375dc768ae1bad2
Jessica Swenson <jessicamswens@gmail.com>
2022-10-12 20:57:33 -0600

    added name problem 9

--- app-9/src/components/Betty.jsx
+++ app-9/src/components/Betty.jsx
@@ -4,6 +4,7 @@ const Betty = () => {
     return (
         <div>
             <img src='https://www.dogtime.com/assets/uploads/2019/12/huskydoodle-mixed-dog-breed-pictures-cover.jpg' alt='huskey poodle' />
+            <p>Betty</p>
         </div>
     )
 };
--- app-9/src/components/Kobuk.jsx
+++ app-9/src/components/Kobuk.jsx
@@ -4,6 +4,7 @@ const Kobuk = () => {
     return (
         <div>
             <img src='https://www.petlandnovi.com/wp-content/uploads/2021/11/Alaskan-Malamute.png' alt='malamute' />
+            <p>Kobuk</p>
         </div>
     )
 };
